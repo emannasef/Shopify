e522bd3a089e737ac7e63d78e2a0749ee71ee00d
EmanNasef <emannasef21@gmail.com>
2023-06-16 20:51:44 +0300

    Home Fav Btn

--- Shopify/Modules/WishList/View/WishListVC.swift
+++ Shopify/Modules/WishList/View/WishListVC.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class WishListVC: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
+}
+extension WishListVC : UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WishListCell", for: indexPath) as! WishListCell
+        
+        cell.wishListTitle.text = "Eman"
+        
+        return cell
+    }
+    
+    
+    
 }
